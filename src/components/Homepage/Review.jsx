ed42c57a8746736f76fdec83e68f99ec250d02cb
chaubeyjaya <jayachaubey03@gmail.com>
2025-05-30 10:29:23 +0530

    change in reviw page by jaya

--- src/components/Homepage/Review.jsx
+++ src/components/Homepage/Review.jsx
@@ -100,8 +100,8 @@ const Review = () => {
             return (
               <div key={index} className="relative w-[240px] h-[380px] shrink-0 box-border">
                 {/* YELLOW TOP CARD: slightly taller and enhanced */}
-                <div className="relative bg-[#FFD54F] h-24 border-b border-gray-300 overflow-hidden shadow-[inset_0_8px_10px_-4px_rgba(0,0,0,0.2),inset_4px_0_6px_-2px_rgba(0,0,0,0.15),inset_-4px_0_6px_-5px_rgba(0,0,0,0.15)]">
-                  <div className="absolute top-0 left-0 w-full h-3 bg-gradient-to-b from-black/30 to-transparent"></div>
+                <div className="relative bg-[#FFD54F] h-24 border-b border-gray-300 overflow-hidden shadow-[inset_0_8px_10px_-4px_rgba(0,0,0,0.1),inset_4px_0_6px_-2px_rgba(0,0,0,0.08),inset_-4px_0_6px_-5px_rgba(0,0,0,0.08)]">
+                  <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-b from-black/10 to-transparent"></div>
                 </div>
 
                 <div className="bg-white rounded-t-[2rem] shadow-md p-4 pt-12 -mt-8 relative z-10 h-[310px] border border-gray-300 border-t-0 flex flex-col justify-between box-border">
