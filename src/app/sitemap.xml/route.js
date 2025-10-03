cb88dff8d67ff88a8c953b2732f09401620f6271
pranalibobade06 <pranalibobade24@gmail.com>
2025-10-03 17:16:37 +0530

    changes

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -3,17 +3,17 @@ const BASE_URL = "https://drshaziawaghoo.com";
 export async function GET() {
   // Static pages
   const staticPages = [
-    "",
-    "aboutus",
-    "specialities",
-    "testimonials-media",
-    "resources",
-    "contactus",
+    "", 
+    "aboutus", 
+    "specialities", 
+    "testimonials-media", 
+    "resources", 
+    "contactus"
   ];
 
   // All articles
   const articles = [
-    "lepocrasy",
+    "laparoscopy",
     "harnia",
     "breastcancer",
     "colorectalsurgery",
@@ -21,7 +21,7 @@ export async function GET() {
     "gisurgery",
     "thyroid",
     "abdonomial",
-    "colorectal",
+    "colorectal"
   ];
 
   // Final articles
@@ -31,28 +31,27 @@ export async function GET() {
   const allPages = [
     ...staticPages.map((page) => `${BASE_URL}/${page}`),
     ...articles.map((slug) => `${BASE_URL}/allartical/${slug}`),
-    ...finalArticles.map((slug) => `${BASE_URL}/finalartical/${slug}`),
+    ...finalArticles.map((slug) => `${BASE_URL}/finalartical/${slug}`)
   ];
 
   // Generate XML sitemap
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${allPages
-    .map(
-      (url) => `
+${allPages
+  .map((url) => `
   <url>
     <loc>${url}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+    <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
-  </url>`
-    )
-    .join("")}
+  </url>
+`).join("")}
 </urlset>`;
 
   return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml",
+      "Cache-Control": "s-maxage=3600, stale-while-revalidate"
     },
   });
 }
